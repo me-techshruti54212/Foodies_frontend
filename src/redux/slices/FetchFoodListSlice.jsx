62f975ce2a2fab5a4df209824ff57751ad726d14
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-09 00:46:40 +0530

    fetch food data added by admin

---
+++ src/redux/slices/FetchFoodListSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const FetchFood = createAsyncThunk("foodlist", async () => {
+  const response = await axios.get(`${import.meta.env.VITE_URL}api/food/list`);
+  // console.log(response.data.data);
+  return response.data.data;
+});
+const FetchFoodListSlice = createSlice({
+  name: "foodlist",
+  initialState: {
+    isLoading: false,
+    data: null,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(FetchFood.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(FetchFood.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload;
+    });
+    builder.addCase(FetchFood.rejected, (state, action) => {
+      console.log(action.payload);
+      state.isError = true;
+    });
+  },
+});
+
+export default FetchFoodListSlice.reducer;
