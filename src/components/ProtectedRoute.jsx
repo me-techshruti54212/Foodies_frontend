4a6c95bea300ea0a98a1eb2ba21f02118265992a
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-09 00:41:08 +0530

    redirecting to Success page

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+const ProtectedRoute = ({ element }) => {
+  const cartItems = useSelector((state) => state.cart.cart);
+  return <div>{cartItems.length > 0 ? element : <Navigate to="/" />}</div>;
+};
+
+export default ProtectedRoute;
