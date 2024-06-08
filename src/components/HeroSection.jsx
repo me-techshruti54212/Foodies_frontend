57c16011caa9d95311f890a298e96e351dc3a885
me-techshruti54212 <shrutiverma.me@gmail.com>
2024-06-09 00:36:30 +0530

    search food item implemented

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import cuate from "../assets/cuate.png";
+import Button from "./Button";
+import { useDispatch } from "react-redux";
+import { filterSearch } from "../redux/slices/SearchSlice";
+const HeroSection = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex  h-[100vh] justify-evenly bg-brand-dark  items-center">
+      <div className="flex flex-col gap-5 ">
+        <h1 className="leading-tight font-semibold tracking-wider text-center lg:text-[4rem]  sm:text-start">
+          Authentic Home
+          <br />
+          food in India
+        </h1>
+        <p className="tracking-wider text-center sm:text-start">
+          Foodies is a courier service in which authentic home
+          <br /> cook food is delivered to a customer.
+        </p>
+        <div className="text-center sm:text-start">
+          <input
+            className="p-4 outline-none  text-black rounded-l-xl lg:w-[20rem] text-capitalize"
+            placeholder="Search food you love"
+            onChange={(e) => dispatch(filterSearch(e.target.value))}
+          />
+          <Button property="p-4 lg:px-7 rounded-r-xl bg-yellow">Search</Button>
+        </div>
+      </div>
+      <img className="w-[35vw] hidden sm:block" src={cuate} />
+    </div>
+  );
+};
+
+export default HeroSection;
